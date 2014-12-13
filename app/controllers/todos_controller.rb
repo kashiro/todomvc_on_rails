718cdaec02c685ddf817b29483d38b6cd6273683
tomoyuki kashiro <ksrtmyk@gmail.com>
2014-12-13 22:07:12 +0900

    add routes and controller init

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,21 @@
+class TodosController < ApplicationController
+
+  def index
+  end
+
+  def toggle
+  end
+
+  def toggle_all
+  end
+
+  def active
+  end
+
+  def completed
+  end
+
+  def destroy_completed
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+
+  root to: "todos#index"
+
+  resources :todos do
+    member do
+      post :toggle
+    end
+
+    collection do
+      post :toggle_all
+      get  :active
+      get  :completed
+      delete :destroy_completed
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
