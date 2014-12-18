16ccf6fe355116d0f2bad658812966f2daba4b7a
tomoyuki kashiro <ksrtmyk@gmail.com>
2014-12-18 17:56:07 +0900

    add controller spec

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -2,12 +2,10 @@ require 'rails_helper'
 
 RSpec.describe TodosController, :type => :controller do
 
-  # 紐づくviewを描画する
-  render_views
-
-  before do
+  before(:each) do
     @task = FactoryGirl.create(:todo)
     @task_completed = FactoryGirl.create(:todo, :completed)
+    request.env["HTTP_ACCEPT"] = "application/javascript"
   end
 
   describe "GET method" do
@@ -20,6 +18,42 @@ RSpec.describe TodosController, :type => :controller do
       expect(response).to be_success
 
       get :completed
+      expect(response).to be_success
+    end
+  end
+
+  describe "POST method" do
+
+    it "should be success" do
+
+      post :toggle, :id => @task
+      expect(response).to be_success
+
+      post :toggle_all
+      expect(response).to be_success
+
+      post :create, todo: {title: "test task"}
+      expect(response).to be_success
+    end
+  end
+
+  describe "DELETE method" do
+
+    it "should be success" do
+
+      delete :destroy_completed
+      expect(response).to be_success
+
+      delete :destroy, :id => @task
+      expect(response).to be_success
+    end
+  end
+
+  describe "PUT method" do
+
+    it "should be success" do
+
+      put :update, :id => @task, todo: {title: "test task"}
       expect(response).to be_success
     end
   end
